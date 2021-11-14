202d37384151d8a15bad40e24bb2e924e760c475
Bart Krakowski <bartlomiej@krakowski.dev>
2021-11-14 17:44:06 +0100

    chore: update structure

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const dist = path.resolve(__dirname, "dist");
 module.exports = {
   mode: "production",
   entry: {
-    index: "./js/index.ts"
+    index: "./ts/index.ts"
   },
   output: {
     path: dist,
